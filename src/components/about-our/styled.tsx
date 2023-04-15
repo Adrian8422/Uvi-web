a40cf12cbe3346a506d37251da64b3144f6c0f8e
Adrian Villegas <manbassman1996@gmail.com>
2023-04-15 19:13:38 -0300

    changes-description

--- src/components/about-our/styled.tsx
+++ src/components/about-our/styled.tsx
@@ -5,7 +5,7 @@ export const ContainerTextAboutOur = styled.div`
   flex-direction: column;
   -webkit-box-align: center;
   align-items: center;
-  max-width: 1000px;
+  width: 100%;
   height: 100%;
   padding: 70px 20px;
   background-color: white;
@@ -17,7 +17,45 @@ export const ContainerImgUvi = styled.div`
   width: 100%;
   justify-content: center;
   background-color: #80008057;
+  @media (min-width: 660px) {
+    justify-content: space-around;
+  }
 `;
 export const ImgUvi = styled.img`
+  display: none;
   width: 174px;
+  @media (min-width: 660px) {
+    display: flex;
+  }
+`;
+export const TitleQuienesSomos = styled.h2`
+  letter-spacing: 3px;
+  font-size: 50px;
+  font-family: "Inter";
+  font-weight: normal;
+`;
+
+export const ContainerTextAndImages = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  width: 100%;
+
+  @media (min-width: 700px) {
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 34px;
+  }
+`;
+
+export const ContainerImageAbout = styled.div`
+  display: flex;
+  width: 250px;
+  height: 250px;
+`;
+export const ImageAbout = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
